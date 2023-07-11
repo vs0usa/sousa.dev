c1b48bb9bc05904eef466c6efa68bd025f2a0a87
rdx <31172722+rd-xx@users.noreply.github.com>
2023-07-11 13:23:19 +0200

    fix(ui): responsiveness issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,19 @@ import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center gap-x-36 bg-black">
+    <main className="flex flex-col sm:flex-row min-h-screen items-center justify-center gap-y-8 sm:gap-x-36 bg-black">
       <div>
         <Image
           src="/cat.gif"
-          width={200}
-          height={200}
           className="rounded-full"
           alt="cat wearing a wig"
+          width={200}
+          height={200}
+          sizes="100vw"
         />
       </div>
-      <div className="flex flex-col items-center gap-y-6 text-gray-100">
-        <h1 className="text-6xl font-semibold">rdx</h1>
+      <div className="flex flex-col items-center sm:gap-y-6 text-gray-100">
+        <h1 className="text-4xl sm:text-6xl font-semibold">rdx</h1>
         <h3 className="text-xl">w/.</h3>
       </div>
     </main>
