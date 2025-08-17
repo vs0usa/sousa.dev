6cae49981bc0fab9420d1dc8f538fa24760df75d
rdx <reydux95@gmail.com>
2025-08-17 11:46:09 +0100

    chore(next): use new ts config

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare"
+import type { NextConfig } from "next"
+
+initOpenNextCloudflareForDev()
+
+const nextConfig: NextConfig = {
+  /* config options here */
+}
+
+export default nextConfig
