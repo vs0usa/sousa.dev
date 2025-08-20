58d49b8f8bf93f47094ab6100be2c548d3f42f12
rdx <reydux95@gmail.com>
2025-08-20 11:38:41 +0100

    fix(ui): change bg color name

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -2,7 +2,7 @@ import { VSousaLogo } from "@/components/icons/vs0usa-logo"
 
 export const Navbar = () => {
   return (
-    <header className="sticky inset-0 top-0 z-50 max-w-screen overflow-x-hidden bg-background px-2 pt-2">
+    <header className="sticky inset-0 top-0 z-50 max-w-screen overflow-x-hidden bg-default px-2 pt-2">
       <div className="mx-auto flex h-12 items-center justify-between gap-2 px-2 screen-lines border-x sm:gap-4 md:max-w-3xl">
         <VSousaLogo className="h-6" />
       </div>
